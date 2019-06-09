f2919ec0df49455fa4c52c8ee18612bf324d9bb2
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-06-09 12:40:15 +0200

    Merging

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,28 @@
 #include "../inc/Game.hpp"
 
 int main(void) {
-    Player player(1, 1);
-    Game game(player);
-    int yMax, xMax;
-    
-    initscr();
-    noecho();
-    cbreak();
+	Player player(1, 1);
+	Game game(player);
+	int yMax, xMax;
+	
+	initscr();
+	noecho();
+	cbreak();
 
 	int start = game.getMilliCount();
 	int milliSecondsElapsed;
-    
-    getmaxyx(stdscr, yMax, xMax);
-    
-    WINDOW *win = game.createWindow(yMax, xMax, 0, 0);
+	
+	getmaxyx(stdscr, yMax, xMax);
+	
+	WINDOW *win = game.createWindow(yMax, xMax, 0, 0);
 	nodelay(win, TRUE);
 
-    box(win, 0, 0);
-    keypad(win, true);
-    curs_set(0);  // hides the default screen cursor.
+	box(win, 0, 0);
+	keypad(win, true);
+	curs_set(0);  // hides the default screen cursor.
 
-    game.setTermDimensions(yMax, xMax);
-    game.menu(win, yMax, xMax);
+	game.setTermDimensions(yMax, xMax);
+	game.menu(win, yMax, xMax);
 
  	while(game.player.alive) {
 		milliSecondsElapsed = game.getMilliSpan(start) / 1000; // grabs current time
@@ -32,11 +32,11 @@ int main(void) {
 		getmaxyx(stdscr, yMax, xMax);
 		game.windowClean(win);
 		mvwprintw(win, 0, xMax /2, "Time: %d", milliSecondsElapsed);
-        game.displayPlayer(win, game.player);
+		game.displayPlayer(win, game.player);
 		game.getAction(win, yMax, xMax);
 		wrefresh(win);
-        usleep(DELAY);
-    }
+		usleep(DELAY);
+	}
 
-    endwin();
+	endwin();
 }
